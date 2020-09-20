809c950ec8c6fe3a463460298e44da218ea23e48
Michael <michael@tricht.dev>
2020-09-20 20:38:00 +0200

    Add flag for passing the application directory in manifest command.

--- appboot/cmd/manifest.go
+++ appboot/cmd/manifest.go
@@ -7,11 +7,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var directory string
 var output string
 var url string
 
 func init() {
 	rootCmd.AddCommand(manifestCmd)
+	manifestCmd.Flags().StringVarP(&directory, "directory", "d", ".", "Path to the application directory")
 	manifestCmd.Flags().StringVarP(&output, "output", "o", "./manifest.json", "Output path for the generated manifest file")
 	manifestCmd.Flags().StringVarP(&url, "url", "u", "", `URL prefix where the files will be hosted and can be downloaded 
 	with a trailing slash`)
@@ -22,12 +24,12 @@ var manifestCmd = &cobra.Command{
 	Use:   "manifest",
 	Short: "Generate a manifest file for your application",
 	Long: `Generate a manifest file for your application. 
-This command currently only works when your application is the working directory.
 Example:
-	cd my-app && appboot manifest --output=./manifest.json --url=https://storage.googleapis.com/my-app/`,
+
+appboot manifest --directory=./my-app/ --output=./manifest.json --url=https://storage.googleapis.com/my-app/`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		log.Println("Generating manifest file...")
-		err := manifest.Generate(".", output, url)
+		err := manifest.Generate(directory, output, url)
 		if err != nil {
 			return err
 		}
